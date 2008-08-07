37487791238305098ea584666715580ff69e064f
ekeeke31 <ekeeke31@8be9ea45-4337-0410-9b5c-d1cd6552648d>
2008-08-07 15:12:26 +0000

    added 080601 version

---
+++ tags/smsplusgx-080601/source/ngc/config.h
@@ -0,0 +1,35 @@
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+typedef struct
+{
+  int sndrate;
+  int country;
+  int console;
+  int display;
+  int fm_enable;
+  int fm_which;
+  int codies;
+  int16 xshift;
+  int16 yshift;
+  int16 xscale;
+  int16 yscale;
+  uint8 aspect;
+  uint8 render;
+  uint8 tv_mode;
+  uint8 overscan;
+  uint8 palette;
+  int8 autofreeze;
+  uint8 use_bios;
+} t_option;
+
+/* Global data */
+t_option option;
+
+
+extern void set_option_defaults ();
+extern void config_load();
+extern void config_save();
+
+#endif /* _CONFIG_H_ */
